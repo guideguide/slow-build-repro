cf43323afa553e904092da50a1f7b3b5c177c5bc
Cameron McEfee <cm@cameronmcefee.com>
2022-10-14 10:15:52 -0700

    remove sentry

--- packages/website/api/_lib/with-sentry.ts
+++ packages/website/api/_lib/with-sentry.ts
@@ -1,15 +1,9 @@
-import * as Sentry from '@sentry/nextjs'
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 function withSentry(fn: Function) {
   return async (req: NextApiRequest, res: NextApiResponse) => {
-    try {
-      return await fn(req, res)
-    } catch (error) {
-      Sentry.captureException(error)
-      res.status(500).send('Error')
-    }
-  }
+    return await fn(req, res);
+  };
 }
 
-export default withSentry
+export default withSentry;
